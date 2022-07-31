4c2357a346b53d7473ee0b48dc321ecd93a9cd14
Chairil ashar <asharchairil4@gmail.com>
2022-07-31 16:51:24 +0700

    belum selesai

---
+++ challenge17/challenge17.mjs
@@ -0,0 +1,40 @@
+export const Pi = 22/7
+
+export default class MesinHitung {
+    constructor() {
+        this.x = 1
+    }
+
+    tambah(value) {
+        this.x += value;
+        return this;
+    }
+    kurang(value) {
+        this.x -= value;
+        return this;
+    }
+    bagi(value) {
+        this.x /= value;
+        return this;
+    }
+    kali(value) {
+        this.x *= value;
+        return this;
+    }
+    pangkat(value) {
+        this.x = this.x ** value
+        return this;
+    }
+    akarPangkat() {
+        this.x = Math.sqrt(this.x)
+        return this;
+    }
+    pangkatDua() {
+        this.x = this.x ** 2
+        return this;
+    }
+
+    result() {
+        console.log(this.x)
+    }
+}
