9d089e117084303b8448cce7042effe5079c51b0
Chairil ashar <asharchairil4@gmail.com>
2022-09-09 14:44:42 +0700

    done

---
+++ lomba17agustus/17agustus.js
@@ -0,0 +1,24 @@
+function test (param){
+    if(param.length < 8){
+        convert = param.slice(0,3)
+        convert2 = param.slice(3,7)
+        convert3 = param.slice(0,7)
+        console.log(convert, convert2)
+        console.log(convert3)
+    }
+    if(param.length > 7 && param.length < 10){
+        convert = param.slice(0,3)
+        convert2 = param.slice(3,7)
+        convert3 = param.slice(7,9)
+        convert4 = param.slice(0,7)
+        convert5 = param.slice(7,9)
+        console.log(convert, convert2,convert3)
+        console.log(convert4,convert5)
+    }
+    if(param.length > 9 && param.length < 13){
+        convert =
+    }
+
+}
+test('program')
+test('programit')
