371e280ece426a3bc0e2a4469c2a785c7759335d
chairil ashar <asharchairil4@gmail.com>
2022-11-26 02:28:28 +0700

    has done

---
+++ challenge6.js
@@ -0,0 +1,16 @@
+function sentenceManipulation(word){
+    let arr = word.split(" ");
+    let newArr= [];
+    arr.forEach(item => {
+        let newString = '';
+        if(item[0] == 'a' || item[0] =='i' || item[0] =='u' || item[0] =='e' || item[0] =='o'){
+            newString = item
+        }else {
+            newString = item.slice(1, item.length)+item[0]+'nyo'
+        }
+        newArr.push(newString);
+    })
+    return newArr.join(" ")
+}
+
+console.log(sentenceManipulation('ibu pergi ke pasar bersama aku'));
